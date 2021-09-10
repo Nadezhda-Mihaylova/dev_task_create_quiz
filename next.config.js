6875f1a54fee877b1e0eb8d1d85d8e205b7fe224
Nadezhda-Mihaylova <64796417+Nadezhda-Mihaylova@users.noreply.github.com>
2021-09-10 11:24:18 +0300

    redirect - home page to quiz

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const { redirect } = require("next/dist/server/api-utils");
+
 module.exports = {
   reactStrictMode: true,
+    async redirect() {
+        return [
+            {
+                source: '/',
+                destination: '/quiz',
+                permanent: true,
+            },
+        ]
+    },
 }
