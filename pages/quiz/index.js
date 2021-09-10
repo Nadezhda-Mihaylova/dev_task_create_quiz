4bc0604da20b9c732873f397502b8850c9e7c4e3
Nadezhda-Mihaylova <64796417+Nadezhda-Mihaylova@users.noreply.github.com>
2021-09-10 11:29:21 +0300

    quiz page

---
+++ pages/quiz/index.js
@@ -0,0 +1,15 @@
+//our-domain.com/quiz
+
+import { Fragment } from "react";
+import Quiz from "../../components/QuizMain/QuizMain";
+
+
+function QuizPage() {
+  return (
+    <Fragment>
+        <Quiz/>
+    </Fragment>
+  );
+}
+
+export default QuizPage;
